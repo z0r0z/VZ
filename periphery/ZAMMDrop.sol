fd13d14e2dd8130618fe8c959658ceacbf32fa0e
z0r0z <zrzr8963@gmail.com>
2025-06-28 22:17:39 +0700

    drop lockups

--- periphery/ZAMMDrop.sol
+++ periphery/ZAMMDrop.sol
@@ -53,4 +53,37 @@ contract ZAMMDrop {
             }
         }
     }
+
+    error InvalidArray();
+
+    function lock(
+        address token,
+        uint256 id,
+        uint256 unlockTime,
+        address[] calldata tos,
+        uint256[] calldata amounts,
+        uint256 totalAmount
+    ) public payable {
+        require(tos.length == amounts.length, InvalidArray());
+        IZAMM(token).transferFrom(msg.sender, address(this), id, totalAmount);
+        for (uint256 i; i != tos.length; ++i) {
+            Z.lockup(token, tos[i], id, amounts[i], unlockTime);
+        }
+    }
+
+    function approve(IZAMM token) public payable {
+        token.setOperator(address(Z), true);
+    }
+}
+
+IZAMM constant Z = IZAMM(0x000000000000040470635EB91b7CE4D132D616eD);
+
+interface IZAMM {
+    function setOperator(address operator, bool approved) external returns (bool);
+    function transferFrom(address from, address to, uint256 id, uint256 amount) external returns (bool);
+
+    function lockup(address token, address to, uint256 id, uint256 amount, uint256 unlockTime)
+        external
+        payable
+        returns (bytes32 lockHash);
 }
